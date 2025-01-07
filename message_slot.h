b380ed5a5f0c303f53f20d4e7673b47785b4ce4a
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2025-01-07 17:16:14 +0200

    Update message_slot.h

--- message_slot.h
+++ message_slot.h
@@ -1,2 +1,21 @@
+#ifndef MESSAGE_SLOT_H
+#define MESSAGE_SLOT_H
+#include <linux/ioctl.h>
 
 #define BUFFER_SIZE 128
+#define MAJOR_NUMBER 235
+#define MESSAGE_SLOT_CHANNEL _IOW(MAJOR_NUMBER, 0, unsigned int)
+#define DEVICE_NAME "message slot"
+
+struct channel_node{
+    unsigned int channel_id;
+    char message[BUFFER_SIZE];
+    int message_len;
+    struct channel *next;
+};
+
+struct message_slot{
+    struct channel *channel_list
+};
+
+#endif
