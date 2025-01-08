da5c90f8d7acc72e664be80d846b6a320bbed5a5
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2025-01-08 09:50:27 +0200

    Update message_slot.c

--- message_slot.c
+++ message_slot.c
@@ -17,15 +17,56 @@ static struct message_slot device_table[257];
 
 static int device_open(struct inode *inode, struct file *file){
     unsigned int minor = iminor(inode);
-    struct message_slot *slot = &device_table[minor];
-    printk("aaaaaaaaaa\n");
+    struct message_slot *slot;
 
-    if (!slot){
-        printk(KERN_ERR "Error: failed to allocate memory for message\n");
-        return -ENOMEM; 
+    if (minor >= 257){
+        printk(KERN_ERR "Error: invalid minor {minor}\n");
+        return -ENODEV;
+    }
+
+    slot = &device_table[minor];
+    if (!slot->channel_list){
+        slot->channel_list = NULL;
     }
 
     file->private_data = slot;
+    return 0;
+}
+
+
+static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned long ioctl_param){
+    struct message_slot *slot;
+    struct channel *chan;
+    unsigned int channel_id = (unsigned int)ioctl_param;;
+
+    if (ioctl_command_id != MSG_SLOT_CHANNEL || channel_id == 0){
+        printk("Error: invalid arguments. either channel ID is 0 or the ioctl command is not MSG_SLOT_CHANNEL\n");
+        return -EINVAL;
+    }
+    
+    slot = file->private_data;
+    
+    
+    chan = slot->channel_list;
+    while (chan){
+        if (chan->id == channel_id){
+            file->private_data = chan;
+            return 0;
+        }
+        chan = chan->next;
+    }
+
+    chan = kmalloc(sizeof(struct channel), GFP_KERNEL);
+    if (!chan){
+        printk("Error: unable to allocate memory\n");
+            return -ENOMEM;
+        }
+    chan->id = channel_id;
+    chan->message_len = 0;
+    chan->next = slot->channel_list;
+    slot->channel_list = chan;
+    
+    file->private_data = chan;
     return 0;
 }
 
@@ -53,8 +94,10 @@ static ssize_t device_write(struct file *file, const char __user *buffer, size_t
         printk(KERN_ERR "Error: failed to copy message\n");
         return -EFAULT;
     }
+    
+    chan->message_len = len;
     printk("wrote messge from channel %d and minor %d", chan->id, iminor(file->f_inode));
-    return chan->message_len;
+    return len;
 }
 
 
@@ -64,6 +107,7 @@ static ssize_t device_read(struct file *file, char __user *buffer, size_t len, l
         printk(KERN_ERR "Error: buffer pointer is NULL");
         return -EINVAL;
     }
+    
     chan = file->private_data;
 
     if (!chan){
@@ -87,46 +131,6 @@ static ssize_t device_read(struct file *file, char __user *buffer, size_t len, l
     }
     printk("read messge from channel %d and minor %d", chan->id, iminor(file->f_inode));
     return chan->message_len;
-}
-
-
-static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned long ioctl_param){
-    struct message_slot *slot;
-    struct channel *chan;
-    unsigned int channel_id;
-    slot = file->private_data;
-    
-
-    channel_id = (unsigned int)ioctl_param;
-
-    if (ioctl_command_id != MSG_SLOT_CHANNEL || channel_id == 0){
-        printk("Error: invalid arguments. either channel ID is 0 or the ioctl command is not MSG_SLOT_CHANNEL\n");
-        return -EINVAL;
-    }
-    
-    chan = slot->channel_list;
-    
-    while (chan){
-        if (chan->id == channel_id){
-            break;
-        }
-        chan = chan->next;
-    }
-
-    if (!chan){
-        chan = kmalloc(sizeof(struct channel), GFP_KERNEL);
-        if (!chan){
-            printk("Error: unable to allocate memory\n");
-            return -ENOMEM;
-        }
-        chan->id = channel_id;
-        chan->message_len = 0;
-        chan->next = slot->channel_list;
-        slot->channel_list = chan;
-    }
-
-    file->private_data = chan;
-    return 0;
 }
 
 
@@ -165,16 +169,17 @@ static int __init message_slot_init(void){
     for (i = 0; i < 257; i++){
         device_table[i].channel_list = NULL;
     }
-
+    printk(KERN_INFO "Message slot module loaded");
     return 0;
 }
 
 static void __exit message_slot_exit(void){
     int i;
-    for (i = 0; i < 256; i++){
+    for (i = 0; i < 257; i++){
         free_massage_slot(&device_table[i]);
     }
     unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
+    printk(KERN_ERR "Message slot unloaded");
 }
 
 module_init(message_slot_init);
