cc4d4fd6cf994b326ea54bde343cee1791ed0be9
daniella1sim <128131945+daniella1sim@users.noreply.github.com>
2025-01-08 10:44:24 +0200

    Update message_slot.c

--- message_slot.c
+++ message_slot.c
@@ -20,16 +20,13 @@ static int device_open(struct inode *inode, struct file *file){
     struct message_slot *slot;
 
     if (minor >= 257){
-        printk(KERN_ERR "Error: invalid minor {minor}\n");
+        printk(KERN_ERR "Error: invalid minor %u\n", minor);
         return -ENODEV;
     }
 
     slot = &device_table[minor];
-    if (!slot->channel_list){
-        slot->channel_list = NULL;
-    }
-
     file->private_data = slot;
+    printk(KERN_INFO "device opened - minor %u\n",minor);
     return 0;
 }
 
@@ -46,11 +43,11 @@ static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsig
     
     slot = file->private_data;
     
-    
     chan = slot->channel_list;
     while (chan){
         if (chan->id == channel_id){
             file->private_data = chan;
+            printk(KERN_INFO "device ioctl found existing channel ID %u\n",channel_id);
             return 0;
         }
         chan = chan->next;
@@ -58,7 +55,7 @@ static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsig
 
     chan = kmalloc(sizeof(struct channel), GFP_KERNEL);
     if (!chan){
-        printk("Error: unable to allocate memory\n");
+        printk(KERN_ERR "Error: unable to allocate memory\n");
             return -ENOMEM;
         }
     chan->id = channel_id;
@@ -67,23 +64,23 @@ static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsig
     slot->channel_list = chan;
     
     file->private_data = chan;
+    printk(KERN_INFO "device ioctl created new channel\n");
     return 0;
 }
 
 
 static ssize_t device_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset){
-    struct channel *chan;
-    if (!buffer){
-        printk(KERN_ERR "Error: buffer pointer is NULL");
-        return -EINVAL;
-    }
-
-    chan = file->private_data;
-
+    struct channel *chan = file->private_data;
+    
     if (!chan){
         printk(KERN_ERR "Error: channel not set correctly\n");
         return -EINVAL;
     } 
+    
+    if (!buffer){
+        printk(KERN_ERR "Error: buffer pointer is NULL\n");
+        return -EINVAL;
+    }
     
     if(len == 0 || len > BUFFER_SIZE){
         printk(KERN_ERR "Error: message len should be between 1 and 128\n");
@@ -96,24 +93,23 @@ static ssize_t device_write(struct file *file, const char __user *buffer, size_t
     }
     
     chan->message_len = len;
-    printk("wrote messge from channel %d and minor %d", chan->id, iminor(file->f_inode));
+    printk(KERN_INFO "wrote messge from channel %u and minor %u of %ld bytes\n", chan->id, iminor(file->f_inode),len);
     return len;
 }
 
 
 static ssize_t device_read(struct file *file, char __user *buffer, size_t len, loff_t *offset){
-    struct channel *chan;
-    if (!buffer){
-        printk(KERN_ERR "Error: buffer pointer is NULL");
-        return -EINVAL;
-    }
+    struct channel *chan = file->private_data;
     
-    chan = file->private_data;
-
     if (!chan){
         printk(KERN_ERR "Error: channel not set correctly\n");
         return -EINVAL;
     } 
+    
+    if (!buffer){
+        printk(KERN_ERR "Error: buffer pointer is NULL");
+        return -EINVAL;
+    }
     
     if(chan->message_len == 0){
         printk(KERN_ERR "Error: no message in channel\n");
@@ -129,7 +125,7 @@ static ssize_t device_read(struct file *file, char __user *buffer, size_t len, l
         printk(KERN_ERR "Error: failed to copy message\n");
         return -EFAULT;
     }
-    printk("read messge from channel %d and minor %d", chan->id, iminor(file->f_inode));
+    printk(KERN_INFO "read messge from channel %d and minor %d", chan->id, iminor(file->f_inode));
     return chan->message_len;
 }
 
@@ -143,13 +139,6 @@ static void free_channels(struct channel *chan){
     }
 }
 
-static void free_massage_slot(struct message_slot *slot){
-    if (slot){
-        free_channels(slot->channel_list);
-        kfree(slot);
-    }
-}
-
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = device_open,
@@ -160,7 +149,9 @@ static struct file_operations fops = {
 
 static int __init message_slot_init(void){
     int i;
-    int ret = register_chrdev(MAJOR_NUMBER, DEVICE_NAME, &fops);
+    int ret;
+    printk(KERN_INFO "messag slot init begin");
+    ret = register_chrdev(MAJOR_NUMBER, DEVICE_NAME, &fops);
     if (ret < 0){
         printk(KERN_ERR "device registration failed for %d\n", MAJOR_NUMBER);
         return ret;
@@ -168,15 +159,26 @@ static int __init message_slot_init(void){
 
     for (i = 0; i < 257; i++){
         device_table[i].channel_list = NULL;
+        printk(KERN_INFO "message slot init:initialized slot %d\n",i);
+            
     }
+
     printk(KERN_INFO "Message slot module loaded");
     return 0;
 }
 
 static void __exit message_slot_exit(void){
     int i;
+    
+    printk(KERN_INFO "message_slot_exit: start\n");
+    
     for (i = 0; i < 257; i++){
-        free_massage_slot(&device_table[i]);
+        if (device_table[i].channel_list){
+            printk(KERN_INFO "message slot exit: freeing channel for minior %d\n",i);
+
+            free_channels(&(device_table[i].channel_list));
+            device_table[i].channel_list = NULL;    
+        }
     }
     unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
     printk(KERN_ERR "Message slot unloaded");
